e96d053c0aaf5492df7ade8a0d9e6cc832acc8d3
Ksiham19 <sihamkhiar18@gmail.com>
2024-10-09 20:20:40 +0200

    Partie code de ex 2

--- src/TP0/EX2/Main.java
+++ src/TP0/EX2/Main.java
@@ -2,22 +2,22 @@ package TP0.EX2;
 
 public class Main {
     public static void main(String[] args) {
-        Compte compteCourant = new CompteCourant("1", "11-12-2020");
-        Compte compteEpargne = new CompteEpargne("2", "10-2-2030");
+        Compte compteCourant = new CompteCourant("Compte1", "11-12-2020");
+        Compte compteEpargne = new CompteEpargne("Compte2", "10-2-2030");
 
         Operation debitOperation = new Operation(200, TypeOperation.Debit);
         Operation creditOperation = new Operation(150, TypeOperation.Credit);
 
         System.out.println("Compte Courant:");
-        System.out.println("Ajout d'une opération de débit : " + compteCourant.addOperation(debitOperation));
-        System.out.println("Montant après débit: " + compteCourant.getMontant());
-        System.out.println("Ajout d'une opération de crédit : " + compteCourant.addOperation(creditOperation));
-        System.out.println("Montant après crédit: " + compteCourant.getMontant());
+        System.out.println("Ajout d'une operation de debit : " + compteCourant.addOperation(debitOperation));
+        System.out.println("Montant après debit: " + compteCourant.getMontant());
+        System.out.println("Ajout d'une operation de credit : " + compteCourant.addOperation(creditOperation));
+        System.out.println("Montant après credit: " + compteCourant.getMontant());
 
         System.out.println("\nCompte Epargne:");
-        System.out.println("Ajout d'une opération de débit : " + compteEpargne.addOperation(debitOperation));
-        System.out.println("Montant après débit: " + compteEpargne.getMontant());
-        System.out.println("Ajout d'une opération de crédit : " + compteEpargne.addOperation(creditOperation));
-        System.out.println("Montant après crédit: " + compteEpargne.getMontant());
+        System.out.println("Ajout d'une operation de debit : " + compteEpargne.addOperation(debitOperation));
+        System.out.println("Montant après debit: " + compteEpargne.getMontant());
+        System.out.println("Ajout d'une operation de credit : " + compteEpargne.addOperation(creditOperation));
+        System.out.println("Montant après credit: " + compteEpargne.getMontant());
     }
 }
