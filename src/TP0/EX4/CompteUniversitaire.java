7c5107d6c280cc023a4e2274498327b510107255
AfraeAfr <afraeafrae01@gmail.com>
2024-10-10 00:27:50 +0100

    Exo04

--- src/TP0/EX4/CompteUniversitaire.java
+++ src/TP0/EX4/CompteUniversitaire.java
@@ -1,14 +1,11 @@
 package TP0.EX4;
 
-public class CompteUniversitaireEnseignent extends CompteUniversitaire {
-    private Enseignent enseignent;
+public abstract class CompteUniversitaire {
+    protected String login;
 
-    public CompteUniversitaireEnseignent(Enseignent enseignent) {
-        this.enseignent = enseignent;
-    }
+    public abstract void genererLogin();
 
-    @Override
-    public void genererLogin() {
-        this.login = this.enseignent.getPrenom() + this.enseignent.getNom() + this.enseignent.getGrade() + "@usms.ma";
+    public String getLogin() {
+        return login;
     }
 }
